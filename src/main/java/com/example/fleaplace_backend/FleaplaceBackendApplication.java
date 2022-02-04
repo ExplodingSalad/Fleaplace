c8ddc7a24f77cca479dcb714fe803adf7a9cda4d
Jan <jan.locher@hotmail.ch>
2022-02-04 17:20:23 +0100

    Initial Commit

---
+++ src/main/java/com/example/fleaplace_backend/FleaplaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fleaplace_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleaplaceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleaplaceBackendApplication.class, args);
+    }
+
+}
